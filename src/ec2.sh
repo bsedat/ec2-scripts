57c384e4c2ca3e74efb226de3c653bf1e5a8112b
Ben Sedat <bsedat@tinfoilsecurity.com>
2015-12-22 11:27:49 -0800

    Don't return exit code 0 when not enough arguments are present

--- src/ec2.sh
+++ src/ec2.sh
@@ -6,7 +6,7 @@ ec2-host() {
   local hostname="$1"
   if [ -z "$hostname" ]; then
     echo "Please specify a Name"
-    return
+    return 1
   fi
 
   aws ec2 describe-instances --filters "Name=tag:Name,Values=$hostname" --query "Reservations[*].Instances[*].[PublicDnsName]" --output text
